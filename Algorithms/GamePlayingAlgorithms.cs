373c3c460a6cc824ad18c1a9e82b4140fb69f8c5
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-05-16 14:06:05 +0300

    feat: write a fully new implementation for the algorithms to cope with the game state class implementation

--- Algorithms/GamePlayingAlgorithms.cs
+++ Algorithms/GamePlayingAlgorithms.cs
@@ -1,91 +1,166 @@
-class GamePlayingAlgorithms
+using System;
+
+public class GamePlayingAlgorithms
 {
-    public int Minimax(int[][] board, int depth, bool maxmizingPlayer){
-        if(depth == 0 || IsTerminalNode(board)){
-            return Evaluate(board);
-        }
-        if(maxmizingPlayer){
-            int bestValue = int.MinValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = Minimax(child, depth - 1, false);
-                bestValue = Math.Max(bestValue, value);
-            }
-            return bestValue;
-        }
-        else{
-            int bestValue = int.MaxValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = Minimax(child, depth - 1, true);
-                bestValue = Math.Min(bestValue, value);
-            }
-            return bestValue;
-        }
-
-    }
-
-    public int AlphaBetaPruning(int[][] board, int depth, int alpha, int beta, bool maxmizingPlayer){
-        if(depth == 0 || IsTerminalNode(board)){
-            return Evaluate(board);
-        }
-        if(maxmizingPlayer){
-            int bestValue = int.MinValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = AlphaBetaPruning(child, depth - 1, alpha, beta, false);
-                bestValue = Math.Max(bestValue, value);
-                alpha = Math.Max(alpha, bestValue);
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            return bestValue;
-        }
-        else{
-            int bestValue = int.MaxValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = AlphaBetaPruning(child, depth - 1, alpha, beta, true);
-                bestValue = Math.Min(bestValue, value);
-                beta = Math.Min(beta, bestValue);
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            return bestValue;
-        }
-    }
-
-    public int AlphaBetaPruningWithIt(int[][] board, int depth, int alpha, int beta, bool maxmizingPlayer){
-        if(depth == 0 || IsTerminalNode(board)){
-            return Evaluate(board);
-        }
-        if(maxmizingPlayer){
-            int bestValue = int.MinValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = AlphaBetaPruningWithIt(child, depth - 1, alpha, beta, false);
-                bestValue = Math.Max(bestValue, value);
-                alpha = Math.Max(alpha, bestValue);
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            return bestValue;
-        }
-        else{
-            int bestValue = int.MaxValue;
-            foreach(int[][] child in GetChildren(board)){
-                int value = AlphaBetaPruningWithIt(child, depth - 1, alpha, beta, true);
-                bestValue = Math.Min(bestValue, value);
-                beta = Math.Min(beta, bestValue);
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            return bestValue;
-        }
-    }
-    public static void Main()
+    GameState state = new GameState();
+    Heuristics heuristics = new Heuristics();
+    public static MoveInfo Minimax(GameState state, int depth)
     {
-        // Minimax Algorithm
-        // Alpha-Beta Pruning
-        // Alpha-Beta Pruning with iterative deepening
+        if (depth <= 0 || state.GameOver)
+        {
+            return Evaluate(state);
+        }
+
+        MoveInfo bestMove = null;
+        int bestScore = int.MinValue;
+
+        foreach (var move in state.LegalMoves.Keys)
+        {
+            GameState nextState = CloneState(state);
+            if (nextState.MakeMove(move, out MoveInfo moveInfo))
+            {
+                MoveInfo opponentMove = Minimax(nextState, depth - 1);
+                int score = -opponentMove.Score;
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = moveInfo;
+                    bestMove.Score = score;
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    public static MoveInfo AlphaBetaPruning(GameState state, int depth)
+    {
+        return AlphaBetaPruning(state, depth, int.MinValue, int.MaxValue, true);
+    }
+
+    private static MoveInfo AlphaBetaPruning(GameState state, int depth, int alpha, int beta, bool maximizingPlayer)
+    {
+        if (depth <= 0 || state.GameOver)
+        {
+            return Evaluate(state);
+        }
+
+        MoveInfo bestMove = null;
+
+        if (maximizingPlayer)
+        {
+            int bestScore = int.MinValue;
+
+            foreach (var move in state.LegalMoves.Keys)
+            {
+                GameState nextState = CloneState(state);
+                if (nextState.MakeMove(move, out MoveInfo moveInfo))
+                {
+                    MoveInfo opponentMove = AlphaBetaPruning(nextState, depth - 1, alpha, beta, false);
+                    int score = -opponentMove.Score;
+
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = moveInfo;
+                        bestMove.Score = score;
+                    }
+
+                    alpha = Math.Max(alpha, bestScore);
+                    if (alpha >= beta)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            int bestScore = int.MaxValue;
+
+            foreach (var move in state.LegalMoves.Keys)
+            {
+                GameState nextState = CloneState(state);
+                if (nextState.MakeMove(move, out MoveInfo moveInfo))
+                {
+                    MoveInfo opponentMove = AlphaBetaPruning(nextState, depth - 1, alpha, beta, true);
+                    int score = -opponentMove.Score;
+
+                    if (score < bestScore)
+                    {
+                        bestScore = score;
+                        bestMove = moveInfo;
+                        bestMove.Score = score;
+                    }
+
+                    beta = Math.Min(beta, bestScore);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    public static MoveInfo AlphaBetaPruningWithIterativeDeepening(GameState state, int maxDepth)
+    {
+        MoveInfo bestMove = null;
+
+        for (int depth = 1; depth <= maxDepth; depth++)
+        {
+            MoveInfo currentMove = AlphaBetaPruning(state, depth);
+            if (currentMove == null)
+                break;
+
+            bestMove = currentMove;
+        }
+
+        return bestMove;
+    }
+
+    private static MoveInfo Evaluate(GameState state)
+    {
+        int coinParityWeight = 1;
+        int mobilityWeight = 2;
+        int cornerWeight = 3;
+        int stabilityWeight = 4;
+
+        int coinParityScore = heuristics.CoinParity(state);
+        int mobilityScore = heuristics.Mobility(state);
+        int cornerScore = heuristics.Corner(state);
+        int stabilityScore = heuristics.Stability(state);
+
+        int weightedScore = coinParityWeight * coinParityScore +
+                            mobilityWeight * mobilityScore +
+                            cornerWeight * cornerScore +
+                            stabilityWeight * stabilityScore;
+
+        return new MoveInfo { Score = weightedScore };
+    }
+
+
+    private static GameState CloneState(GameState state)
+    {
+        // Create a deep copy of the GameState object to avoid modifying the original state.
+        GameState clone = new GameState();
+
+        clone.Board = (Player[,])state.Board.Clone();
+        clone.DiscCount = new Dictionary<Player, int>(state.DiscCount);
+        clone.CurrentPlayer = state.CurrentPlayer;
+        clone.GameOver = state.GameOver;
+        clone.Winner = state.Winner;
+        clone.LegalMoves = new Dictionary<Position, List<Position>>(state.LegalMoves);
+        return clone;
+    }
+
+    public static void Main(){
+        // MiniMax
+        // AlphaBetaPruning
+
     }
 }
+        
